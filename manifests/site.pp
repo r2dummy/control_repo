0d895cd053803017aa663661dee72115490d3e06
r2dummy <mihasa1995@gmail.com>
2020-01-22 15:08:40 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,14 @@
 node default {
+file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'ip-172-31-46-250.eu-west-1.compute.internal' {
   include role::master_server
+}
+
+node 'ip-172-31-43-148.eu-west-1.compute.internal' {
+  include role::app_server
 }
