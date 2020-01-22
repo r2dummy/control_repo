a7b8d9ab8272756886bc6b2972fdeaf1b4f5acfb
r2dummy <mihasa1995@gmail.com>
2020-01-22 15:36:46 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node 'ip-172-31-46-250.eu-west-1.compute.internal' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}/n",
   }
 }
 
