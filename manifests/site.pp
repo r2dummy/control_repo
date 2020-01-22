85a58d6f683f7b70cb312ef24962cea8032f9bed
r2dummy <mihasa1995@gmail.com>
2020-01-22 15:34:02 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,10 @@ file { '/root/README':
 }
 node 'ip-172-31-46-250.eu-west-1.compute.internal' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 
 node 'ip-172-31-43-148.eu-west-1.compute.internal' {
