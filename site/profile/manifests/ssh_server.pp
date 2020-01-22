ff598df63f45588a62d5a083dbefda53888760fb
r2dummy <mihasa1995@gmail.com>
2020-01-22 16:25:18 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
         ensure => present,
         user => 'root',
         type => 'ssh-rsa',
-        key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDH/V3nhN0LSvYFAc8JTWRidbz52ILM29XtWRGyMrpG1V78E4mn2nTfE8/OmXde/rD9HaSowtRN9iz96f0V8jXhUyCbEty28neCsntNiOn9ApPwGDTJtiGEiisRcKY6Vu7W4DioI+3dJE85eAoOpbR+PbS2gzoxUkbVQyABGDrRRTQV/VtbfhnVyNu0Ot15gUmxDp8wT91pSkvpHwzuaGJtCQGRvfVPqqDilMAZovWU8u9MCOG1XsN7nR8BbqUCy3Cc7gTWoWsa28DE0JoZGoNvXamlweNR7lFYqQSL569znK7bdk2+ruiNbIIZiBT9ijPbAaoW9o7t6PKhGIn0gc7v root@ip-172-31-46-250',
+        key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDH/V3nhN0LSvYFAc8JTWRidbz52ILM29XtWRGyMrpG1V78E4mn2nTfE8/OmXde/rD9HaSowtRN9iz96f0V8jXhUyCbEty28neCsntNiOn9ApPwGDTJtiGEiisRcKY6Vu7W4DioI+3dJE85eAoOpbR+PbS2gzoxUkbVQyABGDrRRTQV/VtbfhnVyNu0Ot15gUmxDp8wT91pSkvpHwzuaGJtCQGRvfVPqqDilMAZovWU8u9MCOG1XsN7nR8BbqUCy3Cc7gTWoWsa28DE0JoZGoNvXamlweNR7lFYqQSL569znK7bdk2+ruiNbIIZiBT9ijPbAaoW9o7t6PKhGIn0gc7v root@ip-172-31-46-250',
   }
 }
